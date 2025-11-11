bdcdccb5bbc0de337f8d04bd54a61a1f8bd4f6d8
hallow.ween <sky608479@gmail.com>
2025-11-11 15:06:47 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 const auraName = document.getElementById("auraName");
 const auraRarity = document.getElementById("auraRarity");
 const auraBio = document.getElementById("auraBio");
+const auraSelector = document.getElementById("auraSelector");
+
 
 const auras = [
   { name: "Common", rarity: "1/2", bio: "Very common aura"},
   { name: "Uncommon", rarity: "1/4", bio: "Very uncommon aura" },
   { name: "Good", rarity: "1/5", bio: "Very good aura" }
 ]
+
+
+auraSelector.addEventListener("change", function() {
+const auraSelected = parseInt(auraSelector.value);
+auraName.textContent = auras[auraSelected]["name"];
+auraRarity.textContent = auras[auraSelected]["rarity"];
+auraBio.textContent = auras[auraSelected]["bio"];
+});
